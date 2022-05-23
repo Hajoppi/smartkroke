3a35dc3751cf9e2add071edbd92880832db781f6
Zwenkka <33636340+Zwenkka@users.noreply.github.com>
2022-05-23 20:43:08 +0300

    Testing transformation matrices

--- Henryn puuhastelua/IMU_tests/IMU_tests.ino
+++ Henryn puuhastelua/IMU_tests/IMU_tests.ino
@@ -1,23 +1,50 @@
 #include <Arduino_LSM9DS1.h>
+#include <BasicLinearAlgebra.h>
 #include "SensorFusion.h"
 #include "Arduino.h"
 
-SF fusion;
+using namespace BLA;
 
-float gx, gy, gz, ax, ay, az, mx, my, mz, temp;
+SF fusion;
+#define SS_PIN PB12
+#define NUM_LEDS 5
+#define LED_SPACING 1 //cm
+
+float gx, gy, gz, ax, ay, az, temp;
+
+
+float T[] = {10,10};
+BLA::Matrix<3,NUM_LEDS> ledMatrix;
+BLA::Matrix<1,3> filterX = {1,0,0}; //to show only X coordinate
+BLA::Matrix<1,3> filterY = {0,1,0}; //to show only Y coordinate
+
+
 float pitch, roll, yaw;
 float deltat;
-const float G = 9.81;
-
-#define SS_PIN PB12
 int status;
+const float G = 9.81;
+const float lengthOffset = 0;
 
+void initLedMatrix(){
+  Serial.println(ledMatrix.Cols);
+  Serial.println(ledMatrix.Rows);
+  for(int i = 0; i < NUM_LEDS; i++){
+      ledMatrix(0, i) = 0;
+      ledMatrix(1, i) = i*LED_SPACING;
+      ledMatrix(2, i) = 1;
+}}
 
 void setup() {
-  // serial to display data
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial) {}
 
+  
+  // initialize LED matrix
+  
+  initLedMatrix();
+  // serial to display data
+
+  Serial.print("got here");
   // start communication with IMU
   status = IMU.begin();
   if (status < 0) {
@@ -25,30 +52,33 @@ void setup() {
   }
 }
 
-void printAttitude()
-{
+float getRoll() {
   IMU.readAcceleration(ax, ay, az);
   IMU.readGyroscope(gx, gy, gz);
-
   deltat = fusion.deltatUpdate();
-  fusion.MahonyUpdate(-gy*0.0174532925, gz*0.0174532925, gx*0.0174532925,-ay*9.81,  az*9.81, ax*9.81, deltat);
-  //fusion.MadgwickUpdate(-gy*0.0174532925, gz*0.0174532925, gx*0.0174532925,-ay*9.81,  az*9.81, ax*9.81, deltat);
-  
-  roll = fusion.getRoll();
-  pitch = fusion.getPitch();
-  yaw = fusion.getYaw();
-  
-  //For Serial plotter
-  Serial.print("roll:"); Serial.print(roll); Serial.print(", ");
-  Serial.print("pitch:"); Serial.print(pitch); Serial.print(", ");
-  Serial.print("yaw:"); Serial.print(yaw); Serial.print(", ");
-  Serial.println();
+  // fusion.MahonyUpdate(-gy*DEG_TO_RAD, gz*DEG_TO_RAD, gx*DEG_TO_RAD,-ay*G,  az*G, ax*G, deltat);
+  fusion.MadgwickUpdate(-gy*DEG_TO_RAD, gz*DEG_TO_RAD, gx*DEG_TO_RAD,-ay*G,  az*G, ax*G, deltat); //Mag axis still unsure
+  roll = fusion.getRollRadians();
+  return roll;
 }
 
 void loop() {
+  float roll;
+  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()) {
+  
+    roll = getRoll();
+    BLA::Matrix<3,3> transformationMatrix = {
+    cos(roll),-sin(roll),T[0],
+    sin(roll),cos(roll),T[1],
+    0,0,1};
+    BLA::Matrix<3,NUM_LEDS> newMatrix = transformationMatrix * ledMatrix;
+    
+    
+   
+    Serial << filterX*newMatrix << '\n';
 
-  if (IMU.accelerationAvailable() && IMU.gyroscopeAvailable()){
-    printAttitude();
+    }
+
+    
+    
   }
-     
-}
